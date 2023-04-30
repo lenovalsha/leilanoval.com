c9e900dd9dd6067358f2efeb813b1fbf6c662312
lenovalsha <lenovalsha@gmail.com>
2023-04-29 19:14:39 -0600

    Updates

--- src/components/H1/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const H1 = (props) => {
-  return <h1 className="intro-title">{props.content}</h1>;
-};
-
-export default H1;
--- src/components/ProfileImage/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { imageStyle } from "./style";
-
-const ProfileImage = (props) => {
-  return <img style={imageStyle} src={props.img} alt={props.name} />;
-};
-
-export default ProfileImage;
